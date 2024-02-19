84a2b52e1ba009a12ef86a61663878d6280eebe7
BERSON-MESHAK-J <150615715+BERSON-MESHAK-J@users.noreply.github.com>
2024-02-19 11:09:43 +0530

    Update Binary_Search.js

--- javascript/Binary_Search.js
+++ javascript/Binary_Search.js
@@ -1,1 +1,28 @@
-
+function binarySearch(array, target, low, high) {
+    while (low <= high) {
+      const mid = Math.floor(low + (high - low) / 2);
+  
+      if (array[mid] === target)
+        return mid;
+      else if (array[mid] < target)
+        low = mid + 1;
+      else
+        high = mid - 1;
+    }
+  
+    return -1; // target not found
+  }
+  
+  // Example usage:
+  const array = [3, 4, 5, 6, 7, 8, 9];
+  const target = 8;
+  const low = 0;
+  const high = array.length - 1;
+  
+  // call the binary search method
+  // pass arguments: array, target, index of first and last element
+  const result = binarySearch(array, target, low, high);
+  if (result === -1)
+    console.log("Not found");
+  else
+    console.log("Element found at index " + result);
